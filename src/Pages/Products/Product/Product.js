70b709f7d6d2e16e1f71ffe0a56021ad422e0c0e
MK-Chakraborty <mkchakraborty5@gmail.com>
2021-10-13 18:55:23 +0600

    product img

--- src/Pages/Products/Product/Product.js
+++ src/Pages/Products/Product/Product.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
+    const starArray = [];
+    for(let i=0; i< product.star; i++) {
+        starArray.push(<i class="fas fa-star"></i>)
+    }
     return (
-        <Container fluid>
+        <Container fluid className='ps-0 mb-5 bg-white'>
             <Row>
-                <Col xm={12} sm={12} md={5} lg={4}></Col>
+                <Col xm={12} sm={12} md={5} lg={3}>
+                    <img src={product.img} className='p-2' alt="" />
+                </Col>
+                <Col xm={12} sm={12} md={7} lg={9}>
+                    <div className="text-start p-3">
+                        <h5 className='fw-normal'>{product.name}</h5>
+                        <p className="fw-lighter">By {product.seller}</p>
+                    </div>
+                </Col>
             </Row>
         </Container>
     );
